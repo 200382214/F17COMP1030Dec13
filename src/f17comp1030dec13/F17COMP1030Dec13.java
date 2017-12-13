ba02e713b298b75bc757ab02d5bccaaab7602233
JaretWright <hkkA223>
2017-12-13 14:42:21 -0500

    Warmup exercise added

--- src/f17comp1030dec13/F17COMP1030Dec13.java
+++ src/f17comp1030dec13/F17COMP1030Dec13.java
@@ -11,7 +11,39 @@ public class F17COMP1030Dec13 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String[] presents = new String[5];
+        presents[0]="house";
+        presents[1]="lottery ticket";
+        presents[2]="iPhone X";
+        presents[3]="winter tires";
+        presents[4]="broken laptop";
+        
+        displayArray(presents);
+        displayArrayReverse(presents);
+        displayArrayLowerCase(presents);
     }
     
+    public static void displayArray(String[] array)
+    {
+        for (int i=0; i<array.length; i++)
+            System.out.printf("%-20s", array[i]);
+        
+        System.out.println();
+    }
+    
+    public static void displayArrayReverse(String[] array)
+    {
+        for (int i=array.length-1; i>=0; i--)
+            System.out.printf("%-20s", array[i]);
+        
+        System.out.println();
+    }
+ 
+    public static void displayArrayLowerCase(String[] array)
+    {
+        for (int i=0; i<array.length; i++)
+            System.out.printf("%-20s", array[i].toLowerCase());
+        
+        System.out.println();
+    }
 }
